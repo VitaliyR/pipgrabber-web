92270d2651d8523fd615d2d62be5ccd7e11f9891
Vitaliy Ribachenko <vit@ribachenko.com>
2017-11-18 12:31:54 +0200

    Fix voting

--- src/core/features.js
+++ src/core/features.js
@@ -3,7 +3,7 @@ const log = require('./lib/log')('FeaturesController');
 
 function serializeFeature(feature, ctx) {
   const votes = ctx.storage.data.votes[feature.id] || [];
-  const isVoted = votes.indexOf(ctx.ip) >= 0;
+  const isVoted = votes.indexOf(ctx.realIP) >= 0;
   return {
     name: feature.name,
     votes: feature.votes,
@@ -38,7 +38,7 @@ async function getFeature(ctx, next) {
   const feature = ctx.storage.data.features[featureId];
 
   if (!feature) {
-    log.warn(`${ctx.ip} tried to vote for unexisting feature ${featureId}`);
+    log.warn(`${ctx.realIP} tried to vote for unexisting feature ${featureId}`);
     return ctx.respond(ctx, 'err', false);
   }
 
@@ -50,21 +50,21 @@ async function getFeature(ctx, next) {
 
   ctx.feature = feature;
   ctx.votes = votes;
-  ctx.isVoted = votes.indexOf(ctx.ip) >= 0;
+  ctx.isVoted = votes.indexOf(ctx.realIP) >= 0;
 
   return next();
 }
 
 async function voteFeature(ctx) {
   if (ctx.isVoted) {
-    log.warn(`${ctx.ip} tried to vote again for feature ${ctx.feature.id}: ${ctx.feature.name}`);
+    log.warn(`${ctx.realIP} tried to vote again for feature ${ctx.feature.id}: ${ctx.feature.name}`);
     return ctx.respond(ctx, 'err', true);
   }
 
   ctx.feature.votes += 1;
-  ctx.votes.push(ctx.ip);
+  ctx.votes.push(ctx.realIP);
 
-  log.info(`${ctx.ip} voted for ${ctx.feature.id}: ${ctx.feature.name}`);
+  log.info(`${ctx.realIP} voted for ${ctx.feature.id}: ${ctx.feature.name}`);
 
   await ctx.storage.persist();
 
@@ -74,16 +74,16 @@ async function voteFeature(ctx) {
 async function unvoteFeature(ctx) {
   if (ctx.isVoted) {
     ctx.feature.votes -= 1;
-    ctx.votes.splice(ctx.votes.indexOf(ctx.ip), 1);
+    ctx.votes.splice(ctx.votes.indexOf(ctx.realIP), 1);
 
-    log.info(`${ctx.ip} removed vote from ${ctx.feature.id}: ${ctx.feature.name}`);
+    log.info(`${ctx.realIP} removed vote from ${ctx.feature.id}: ${ctx.feature.name}`);
 
     await ctx.storage.persist();
 
     return ctx.respond(ctx, 'ok', false, { feature: serializeFeature(ctx.feature, ctx) });
   }
 
-  log.warn(`${ctx.ip} tried to unvote for feature which he not voted ${ctx.feature.id}: ${ctx.feature.name}`);
+  log.warn(`${ctx.realIP} tried to unvote for feature which he not voted ${ctx.feature.id}: ${ctx.feature.name}`);
   return ctx.respond(ctx, 'err', false);
 }
 
@@ -97,9 +97,15 @@ async function presentMainPage(ctx) {
   return ctx.render('index.hbs', { features });
 }
 
+async function getIP(ctx, next) {
+  ctx.realIP = ctx.req.headers['x-real-ip'] || ctx.ip;
+  await next();
+}
+
 /**
  * Router
  */
+router.use(getIP);
 router.get('/', presentMainPage);
 router.post('/features/:id', getFeature, voteFeature);
 router.del('/features/:id', getFeature, unvoteFeature);
